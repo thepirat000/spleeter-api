9fda02005beeb07f30423ae06eeb859d6e7bbae5
thepirat000 <thepirat000@hotmail.com>
2019-12-02 18:03:01 -0600

    fix

--- docs/splitter.js
+++ docs/splitter.js
@@ -2,11 +2,11 @@ var buttonSplit = $("#btn-split");
 
 var max_duration_mins = 30;
 
-var split_yt_api = 'https://localhost:5001/yt';
-var split_mp3_api = 'https://localhost:5001/mp3'; 
+//var split_yt_api = 'https://localhost:5001/yt';
+//var split_mp3_api = 'https://localhost:5001/mp3'; 
 
-//var split_yt_api = 'https://spleeter-gpu2.eastus.cloudapp.azure.com/yt';
-//var split_mp3_api = 'https://spleeter-gpu2.eastus.cloudapp.azure.com/mp3';
+var split_yt_api = 'https://spleeter-gpu2.eastus.cloudapp.azure.com/yt';
+var split_mp3_api = 'https://spleeter-gpu2.eastus.cloudapp.azure.com/mp3';
 
 
 var selectedFiles = [];
