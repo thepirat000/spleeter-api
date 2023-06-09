9c799e52818b5955a3fa2bd879b8fdf8dc921d31
Federico Colombo <Federico.Colombo.-ND@disney.com>
2023-06-09 14:18:54 -0600

    cert

--- SetupNew.ps1
+++ SetupNew.ps1
@@ -1,8 +1,7 @@
-
 $down = New-Object System.Net.WebClient
 
 # Create restore point
-Checkpoint-Computer -Description 'Before spleeter-gpu'
+Checkpoint-Computer -Description 'Before spleeter'
 
 # Install chocolatey
 iex ($down.DownloadString('https://chocolatey.org/install.ps1'));
@@ -15,6 +14,14 @@ $down.DownloadFile($url,$file);
 # Install dotnet core runtime & hosting bundle
 Write-Host "Install dotnet core runtime & hosting bundle", $file -foregroundcolor "green";
 & $file /install /passive 
+
+# Install dotnet core SDK
+Write-Host "Install dotnet core SDK", $PSScriptRoot -foregroundcolor "green";
+$url  = 'https://download.visualstudio.microsoft.com/download/pr/974313ac-3d89-4c51-a6e8-338d864cf907/6ed5d4933878cada1b194dd1084a7e12/dotnet-sdk-7.0.302-win-x64.exe';
+$file = $PSScriptRoot + '\dotnet-sdk-7.0.302-win-x64.exe';
+$down.DownloadFile($url,$file);
+& $file /install /passive
+
 
 #GIT
 Write-Host "Installing GIT" -foregroundcolor "green";
@@ -69,7 +76,7 @@ git clone -q https://github.com/thepirat000/spleeter-api
 cd spleeter-api
 dotnet build SpleeterAPI.sln -c Release
 dotnet publish SpleeterAPI.csproj -c Release
-cd bin\Release\netcoreapp3.0\publish
+cd bin\Release\net7.0\publish
 
 
 Write-Host "Installation complete..."
