266f5fb39852639fc67b84d1c196ce0bc0c24227
Federico Colombo <Federico.Colombo.-ND@disney.com>
2024-11-02 17:54:08 -0600

    moving docs

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-dotnet bin\Release\net8.0\publish\SpleeterAPI.dll --launch-profile https --urls "https://*:443"
+dotnet bin\Release\net8.0\publish\SpleeterAPI.dll --launch-profile https --urls "http://*:80;https://*:443"
